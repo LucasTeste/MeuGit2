bdf881d6e3ab86d6b7d3738c8bccec8f144cd43a
Lucas <Lucaslegey@gmail.com>
2014-08-01 19:23:36 -0300

    segundaAlteracao

--- testeGit2/src/com/example/testegit2/MainActivity.java
+++ testeGit2/src/com/example/testegit2/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		String primeiraAlteracao;
-		
+		String primeiraAlteracao = "0" ;
+		System.out.print(primeiraAlteracao);
 	}
 
 	@Override
